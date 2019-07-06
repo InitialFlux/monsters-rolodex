8fb35e36e36bedb0f49e4b19437a0c7ba14f3b9b
Dylan <initialflux@gmail.com>
2019-07-06 17:02:43 -0500

    Updates

---
+++ precache-manifest.b12eb37f1ea49e5c13900e80c081175d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83fedeceb7f866c081387a2c884a1d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3fd853b506507aa62098",
+    "url": "/monsters-rolodex/static/css/main.915b8f80.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3fd853b506507aa62098",
+    "url": "/monsters-rolodex/static/js/main.7a119fb5.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
